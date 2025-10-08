36d1f99f8a5fbaf0ab186ceafe2daf58f640f10f
prasanrahulritgit <prasanrahulritgit@gmail.com>
2025-10-08 16:03:51 +0530

    pull the new changes

--- forms.py
+++ forms.py
@@ -5,3 +5,7 @@ from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+
+print("changes")
+    
